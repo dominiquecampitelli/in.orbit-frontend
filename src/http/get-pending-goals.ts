c58778b06cfd1b9e316d39438b9419a8f66f47b0
Dominique Campitelli <dominiquecamp@outlook.com>
2024-09-12 20:33:04 -0300

    Routes adjusment

--- src/http/get-pending-goals.ts
+++ src/http/get-pending-goals.ts
@@ -6,7 +6,7 @@ type PendingGoalsResponse = {
 }[];
 
 export async function getPendingGoals(): Promise<PendingGoalsResponse> {
-  const response = await fetch("http://localhost:3333/summary");
+  const response = await fetch("http://localhost:3333/pending-goals");
   const data = await response.json();
 
   return data.pendingGoals;
